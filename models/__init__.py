30d3276bad320c4d6f64952566032fcc0ee963ce
hhafzahh <2001231F@student.tp.edu.sg>
2026-04-15 23:46:01 +0800

    feat: implemented UNet Model without skip connections

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .baselineunet import UNetClassifier
 from .attentionunet import AUNetClassifier
 from .residualunet import ResUNetClassifier
 from .aresunet import AResUNetClassifier
+from .noskipunet import NoSkipUNetClassifier
 
-__all__ = ["UNetClassifier", "AUNetClassifier", "ResUNetClassifier", "AResUNetClassifier"]
+__all__ = ["UNetClassifier", "AUNetClassifier", "ResUNetClassifier", "AResUNetClassifier", "NoSkipUNetClassifier"]
