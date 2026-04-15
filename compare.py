4d7f3e003173d34dbf951e93c22f8d14f3a30e9e
Potacchi <Innamouse.singapore@gmail.com>
2026-04-15 23:28:14 +0800

    feat + test: testing for normalized dataset
    - changed main.ipynb to normalization.ipynb which showcases the results of model with non-normalized dataset against model with normalized dataset
    - refactored compare.py to fulfill DRY and SRP principles after adding new `compare_models()` function

--- compare.py
+++ compare.py
@@ -32,6 +32,7 @@ def compare_architecture(train_dataloader,
     else:
         if (reload):
             shutil.rmtree(f"{model_dir}/")
+            os.mkdir(model_dir)
             
     for i in range(n):
         curr_name = model_names[i]
@@ -42,22 +43,15 @@ def compare_architecture(train_dataloader,
             if model_names[i] in file:
                 load_model = True
                 model_list[i] = torch.load(f"{model_dir}/{file}", weights_only=False)
-        
+
         # Train model if unable to load anything
         if (not load_model):
-            print(f"Training {curr_name} model...")
-            train_model(model_list[i], criterion, train_dataloader, validation_dataloader, device, verbose=False)
-            print("Training finished, saving model...")
-            save_model(model_list[i], curr_name, 20, model_dir)
+            train_save_model(model_list[i], model_dir, criterion, train_dataloader, validation_dataloader, device, verbose=False)
         else:
             print(f"Successfully retrieved {curr_name} model...")
         
         # Prepare metrics for evaluation
-        print(f"Evaluating {curr_name} model...")
-        train_metrics.append(evaluate_model(model_list[i], train_dataloader, "{}/{}/train".format(eval_dir, model_names[i]), device))
-        validation_metrics.append(evaluate_model(model_list[i], validation_dataloader, "{}/{}/val".format(eval_dir, model_names[i]), device))
-        test_metrics.append(evaluate_model(model_list[i], test_dataloader, "{}/{}/test".format(eval_dir, model_names[i]), device))
-        print("Evaluation finished...")
+        save_metrics(model_list[i], train_dataloader, validation_dataloader, test_dataloader, device, eval_dir)
         
         # Clearing space for other operations
         model_list[i] = None
@@ -66,13 +60,67 @@ def compare_architecture(train_dataloader,
     clear_output() # Clear Print to make way for Evaluation results
     
     for i in range(n):
-        name = model_names[i]
-        print("{}".format("-"*len(name))       
-              + f"\n{name}"        
-              + "\n{}".format("-"*len(name))        
-              + f"\nTrain | Accuracy: {train_metrics[i][0]:.2f}% | F1_Score: {train_metrics[i][1]:.2f} | F2_Score: {train_metrics[i][2]:.2f} | Anomaly Detection: {train_metrics[i][3]:.2f}%"
-              + f"\n  Val | Accuracy: {validation_metrics[i][0]:.2f}% | F1_Score: {validation_metrics[i][1]:.2f} | F2_Score: {validation_metrics[i][2]:.2f} | Anomaly Detection: {validation_metrics[i][3]:.2f}%"
-              + f"\n Test | Accuracy: {test_metrics[i][0]:.2f}% | F1_Score: {test_metrics[i][1]:.2f} | F2_Score: {test_metrics[i][2]:.2f} | Anomaly Detection: {test_metrics[i][3]:.2f}%")
+        print_metrics(model_names[i], train_metrics[i], validation_metrics[i], test_metrics[i])
 
-def compare_models():
-    pass
+# Assuming that the models here are all already trained
+def compare_models(models, train_dataloader, validation_dataloader, test_dataloader, device=None):
+    
+    model_list = []
+    train_metrics = []
+    validation_metrics = []
+    test_metrics = []
+
+    # If models is a string of the dir containing the models to be compared
+    if isinstance(models, str):
+        if (os.path.exists(models)):
+            for model in os.listdir(models):
+                try:
+                    model_list.append(torch.load(model, weights_only=False))
+                except Exception as e:
+                    print("Unable to load model.", e)
+                    return
+        else:
+            print("Error: Model directory does not exist.")
+            return
+    else:
+        model_list = models
+    
+    for model in model_list:
+        train, val, test = save_metrics(model, train_dataloader, validation_dataloader, test_dataloader, device)
+        train_metrics.append(train)
+        validation_metrics.append(val)
+        test_metrics.append(test)
+    
+    for i in range(len(model_list)):
+        print_metrics(model_list[i], train_metrics[i], validation_metrics[i], test_metrics[i])
+    
+
+def train_save_model(model, model_dir, criterion, train_dataloader, validation_dataloader, device, verbose=False):
+    model_name = model._name
+    print(f"Training {model._name} model...")
+    train_model(model, criterion, train_dataloader, validation_dataloader, device, verbose=verbose)
+    print("Training finished, saving model...")
+    save_model(model, model_name, 20, model_dir)
+        
+def save_metrics(model, train_dataloader, validation_dataloader, test_dataloader, device=None, eval_dir="./outputs/training/compare/out"):
+    # Prepare metrics for evaluation comparison
+    model_name = model._name
+    print(f"Evaluating {model_name} model...")
+    train_metrics = (evaluate_model(model, train_dataloader, "{}/{}/train".format(eval_dir, model_name), device))
+    validation_metrics = (evaluate_model(model, validation_dataloader, "{}/{}/val".format(eval_dir, model_name), device))
+    test_metrics = (evaluate_model(model, test_dataloader, "{}/{}/test".format(eval_dir, model_name), device))
+    print("Evaluation finished...")
+
+    return train_metrics, validation_metrics, test_metrics
+      
+def print_metrics(model, train_metrics, validation_metrics, test_metrics):
+    if isinstance(model, str):
+        name = model
+    else:
+        name = model._name
+    print("{}".format("-"*len(name))       
+            + f"\n{name}"        
+            + "\n{}".format("-"*len(name))        
+            + f"\nTrain | Accuracy: {train_metrics[0]:.2f}% | F1_Score: {train_metrics[1]:.2f} | F2_Score: {train_metrics[2]:.2f} | Anomaly Detection: {train_metrics[3]:.2f}%"
+            + f"\n  Val | Accuracy: {validation_metrics[0]:.2f}% | F1_Score: {validation_metrics[1]:.2f} | F2_Score: {validation_metrics[2]:.2f} | Anomaly Detection: {validation_metrics[3]:.2f}%"
+            + f"\n Test | Accuracy: {test_metrics[0]:.2f}% | F1_Score: {test_metrics[1]:.2f} | F2_Score: {test_metrics[2]:.2f} | Anomaly Detection: {test_metrics[3]:.2f}%")
--- utils/loader.py
+++ utils/loader.py
@@ -46,9 +46,14 @@ def import_dataset():
         shutil.rmtree(complete_path)
         print("Removed .complete folder")
 
-def initialize_dataset():
+def initialize_dataset(normalized: bool = False):
     # For loading the Diabetic Retinopathy Dataset
-    transforms_base = transforms.Compose([transforms.ToTensor()])
+    if (normalized):
+        transforms_base = transforms.Compose([transforms.ToTensor(),
+                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                                               std=[0.229, 0.224, 0.225])]) # Normalization values from Pytorch's recommended ImageNet values
+    else:
+        transforms_base = transforms.Compose([transforms.ToTensor()])
 
     # full_dataset[i] is a a tuple of (image tensor, class index)
     full_dataset = torchvision.datasets.DatasetFolder(root="./dataset", loader=torchvision.datasets.folder.default_loader, transform=transforms_base, extensions=[".png"])
